c1bfe1586cccf13b9d6326d1bf1efa2e1d1a63b3
垣尾 颯志 <83001142+kaki005@users.noreply.github.com>
2024-12-18 16:19:41 +0900

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,21 @@
+import hydra
+import omegaconf
+import wandb
+from utilpy import log_init
+
+from configs import Config
+
+
+@hydra.main(version_base=None, config_path="configs/", config_name="default")
+def main(cfg: Config):
+    log_init()
+    wandb.login()
+    wandb.config = omegaconf.OmegaConf.to_container(cfg, resolve=True, throw_on_missing=True)
+    wandb.init(entity=cfg.wandb.entity, project=cfg.wandb.project)
+    loss = 0
+    wandb.log({"loss": loss})
+    wandb.finish()
+
+
+if __name__ == "__main__":
+    main()
